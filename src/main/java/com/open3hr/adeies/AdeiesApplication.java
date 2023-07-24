e1afdf05ae4a23fd3361466f6480bd5da411590e
sspirou <sspirou@ots.gr>
2023-07-24 12:40:34 +0300

    Initial commit

---
+++ src/main/java/com/open3hr/adeies/AdeiesApplication.java
@@ -0,0 +1,13 @@
+package com.open3hr.adeies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdeiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdeiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/open3hr/adeies/AdeiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open3hr.adeies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdeiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
