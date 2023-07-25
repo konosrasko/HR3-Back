c57d8e6741af28d8dd355f12b33a45843cfcf9cb
Stamatis Chatzis <schatzis@ots.gr>
2023-07-25 15:55:10 +0300

    impl request

--- src/main/java/com/open3hr/adeies/dto/LeaveBalanceDTO.java
+++ src/main/java/com/open3hr/adeies/dto/LeaveBalanceDTO.java
@@ -1,6 +1,7 @@
 package com.open3hr.adeies.dto;
 
 import com.open3hr.adeies.entities.LeaveBalance;
+import com.open3hr.adeies.entities.LeaveCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +15,12 @@ public class LeaveBalanceDTO {
     private Integer id;
     private Integer days;
     private Integer daysTaken;
+//    private LeaveCategory category;
 
     public LeaveBalanceDTO(LeaveBalance leaveBalance){
         this.id = leaveBalance.getId();
         this.days = leaveBalance.getDays();
         this.daysTaken = leaveBalance.getDaysTaken();
+//        this.category = leaveBalance.getLeaveCategory();
     }
 }
--- src/main/java/com/open3hr/adeies/entities/LeaveCategory.java
+++ src/main/java/com/open3hr/adeies/entities/LeaveCategory.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,6 +24,13 @@ public class LeaveCategory {
 
     @Column(name="title")
     private String title;
+
+//    @OneToMany(mappedBy = "leaveCategory",
+//            fetch = FetchType.EAGER,
+//            cascade = {
+//                    CascadeType.ALL
+//            })
+//    private List<LeaveBalance> balances = new ArrayList<>();
 
     public LeaveCategory(LeaveCategoryDTO leaveCategoryDTO){
         this.id = leaveCategoryDTO.getId();
