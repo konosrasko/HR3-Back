619f088c4d633d46656acbfb5e8cdde5c6379f92
tvasiliadis <tvasiliadis@ots.gr>
2023-08-11 09:09:51 +0300

    db

--- src/main/resources/db/scripts/demo_data.sql
+++ src/main/resources/db/scripts/demo_data.sql
@@ -1,24 +1,34 @@
-INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `address`, `mobile_number`, `hire_date`, `is_enabled`) VALUES (1, 'Stamatis', 'Chatzis', 'schatzis@ots.gr', 'Panadreoy 164 Neapoli', '2310632434', '2023-04-01', true);
-INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `address`, `mobile_number`, `hire_date`, `is_enabled`) VALUES (2, 'Paylos', 'Tsigaros', 'ptsigar@ots.gr', 'Magnisias 23 Stayroypoli', '6983957541', '2023-03-01', true);
-INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `address`, `mobile_number`, `hire_date`, `is_enabled`) VALUES (3, 'Aretoympa', 'Aretoulidou', 'adimatati@ots.gr', 'Melenikoy 20', '6987142133', '2023-05-01', true);
-INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `address`, `mobile_number`, `hire_date`, `is_enabled`) VALUES (4, 'Mixail', 'Thief', 'klevwotivrw@ots.gr', 'Sikelianoy 51 Dendropotamos', '6969696969', '2023-04-01', true);
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `address`, `mobile_number`, `hire_date`, `is_enabled`,`supervisor_id`) VALUES (1, 'Σταμάτης', 'Χατζής', 'schatzis@ots.gr', 'Παπανδρέου 164 ', '2310632434', '2023-04-01', true, 1);
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `address`, `mobile_number`, `hire_date`, `is_enabled`,`supervisor_id`) VALUES (2, 'Βλοσπα', 'Γάρος', 'ptsigar@ots.gr', 'Στη στιγμή', '6983957541', '2023-03-01', true, 1);
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `address`, `mobile_number`, `hire_date`, `is_enabled`,`supervisor_id`) VALUES (5, 'Ευη', 'Αγχωμένη', 'evi.stressed@ots.gr', 'Μοναστηρίου 60', '6969696969', '2021-04-01', true,2);
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `address`, `mobile_number`, `hire_date`, `is_enabled`,`supervisor_id`) VALUES (3, 'Αρετούλα', 'Αρετουλίδου', 'adimatati@ots.gr', 'Μελενίνου 20', '6987142133', '2023-05-01', true,2);
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `email`, `address`, `mobile_number`, `hire_date`, `is_enabled`,`supervisor_id`) VALUES (4, 'Μιχαήλ', 'Ελβενός', 'klevwotivrw@ots.gr', 'Σπίτι σου', '6969696969', '2023-04-01', true,5);
 
-INSERT INTO `leave_category` (`id`, `title`) VALUES (1, 'Kanoniki');
-INSERT INTO `leave_category` (`id`, `title`) VALUES (2, 'Egkymonsinis');
-INSERT INTO `leave_category` (`id`, `title`) VALUES (3, 'Aimodosias');
+
+
+INSERT INTO `leave_category` (`id`, `title`) VALUES (1, 'Κανονική');
+INSERT INTO `leave_category` (`id`, `title`) VALUES (2, 'Γάμου');
+INSERT INTO `leave_category` (`id`, `title`) VALUES (3, 'Αιμοδοσίας');
+
+
 
 INSERT INTO `leave_balance` (`id`, `employee_id`, `leave_category_id`, `days`, `days_taken`) VALUES (1, 1, 1, 15, 0);
-INSERT INTO `leave_balance` (`id`, `employee_id`, `leave_category_id`, `days`, `days_taken`) VALUES (2, 1, 3, 5, 0);
-INSERT INTO `leave_balance` (`id`, `employee_id`, `leave_category_id`, `days`, `days_taken`) VALUES (3, 2, 2, 25, 0);
-INSERT INTO `leave_balance` (`id`, `employee_id`, `leave_category_id`, `days`, `days_taken`) VALUES (4, 3, 1, 10, 0);
+INSERT INTO `leave_balance` (`id`, `employee_id`, `leave_category_id`, `days`, `days_taken`) VALUES (2, 1, 1, 15, 0);
+INSERT INTO `leave_balance` (`id`, `employee_id`, `leave_category_id`, `days`, `days_taken`) VALUES (3, 2, 1, 15, 0);
+INSERT INTO `leave_balance` (`id`, `employee_id`, `leave_category_id`, `days`, `days_taken`) VALUES (4, 2, 1, 15, 5);
 
-INSERT INTO `leave_request` (`id`, `employee_id`, `leave_category_id`, `submit_date`, `start_date`, `end_date`, `duration`, `status`) VALUES (1, 1, 1, '2023-05-30 12:00:00', '2023-06-01', '2023-07-01', 30, 'PENDING');
-INSERT INTO `leave_request` (`id`, `employee_id`, `leave_category_id`, `submit_date`, `start_date`, `end_date`, `duration`, `status`) VALUES (2, 2, 2, '2023-05-02 10:00:00', '2023-06-01', '2023-07-01', 30, 'PENDING');
-INSERT INTO `leave_request` (`id`, `employee_id`, `leave_category_id`, `submit_date`, `start_date`, `end_date`, `duration`, `status`) VALUES (3, 3, 3, '2023-04-13 12:40:30', '2023-06-01', '2023-07-01', 30, 'PENDING');
-INSERT INTO `leave_request` (`id`, `employee_id`, `leave_category_id`, `submit_date`, `start_date`, `end_date`, `duration`, `status`) VALUES (4, 4, 2, '2023-05-30 12:00:00', '2023-06-01', '2023-07-01', 30, 'PENDING');
-INSERT INTO `leave_request` (`id`, `employee_id`, `leave_category_id`, `submit_date`, `start_date`, `end_date`, `duration`, `status`) VALUES (5, 1, 1, '2023-04-30 12:00:00', '2023-06-01', '2023-07-01', 30, 'PENDING');
 
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `employee_id`, `role`) VALUES (1, 'schatzis', '$2a$12$HbC4Qa3XDqoUIxtia5o19OptfHIeQccF1kWapk9yRztFlafnwoE4i', true, 1, 'Admin');
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `employee_id`, `role`) VALUES (2, 'pablo', '$2a$12$HbC4Qa3XDqoUIxtia5o19OptfHIeQccF1kWapk9yRztFlafnwoE4i', true, 2, 'HR');
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `employee_id`, `role`) VALUES (3, 'areti', '$2a$12$HbC4Qa3XDqoUIxtia5o19OptfHIeQccF1kWapk9yRztFlafnwoE4i', true, 3, 'Employee' );
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `employee_id`, `role`) VALUES (4, 'kleftis', '$2a$12$HbC4Qa3XDqoUIxtia5o19OptfHIeQccF1kWapk9yRztFlafnwoE4i', true, 4, 'Employee' );
+
+
+INSERT INTO `leave_request` (`id`, `employee_id`, `leave_category_id`, `submit_date`, `start_date`, `end_date`, `duration`, `status`) VALUES (1, 1, 1, '2023-05-30 12:00:00', '2023-06-01', '2023-07-01', 30, 'DENIED');
+INSERT INTO `leave_request` (`id`, `employee_id`, `leave_category_id`, `submit_date`, `start_date`, `end_date`, `duration`, `status`) VALUES (2, 2, 2, '2023-05-02 10:00:00', '2023-06-01', '2023-07-01', 30, 'DENIED');
+INSERT INTO `leave_request` (`id`, `employee_id`, `leave_category_id`, `submit_date`, `start_date`, `end_date`, `duration`, `status`) VALUES (3, 3, 3, '2023-04-13 12:40:30', '2023-06-01', '2023-07-01', 30, 'DENIED');
+INSERT INTO `leave_request` (`id`, `employee_id`, `leave_category_id`, `submit_date`, `start_date`, `end_date`, `duration`, `status`) VALUES (4, 4, 2, '2023-05-30 12:00:00', '2023-06-01', '2023-07-01', 30, 'DENIED');
+INSERT INTO `leave_request` (`id`, `employee_id`, `leave_category_id`, `submit_date`, `start_date`, `end_date`, `duration`, `status`) VALUES (5, 1, 1, '2023-04-30 12:00:00', '2023-06-01', '2023-07-01', 30, 'DENIED');
+
+
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `employee_id`, `role`,`is_supervisor`) VALUES (1, 'schatzis', '$2a$12$HbC4Qa3XDqoUIxtia5o19OptfHIeQccF1kWapk9yRztFlafnwoE4i', true, 1, 'Admin',1);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `employee_id`, `role`,`is_supervisor`) VALUES (2, 'pablo', '$2a$12$HbC4Qa3XDqoUIxtia5o19OptfHIeQccF1kWapk9yRztFlafnwoE4i', true, 2, 'HR',1);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `employee_id`, `role`,`is_supervisor`) VALUES (5, 'evihr', '$2a$12$HbC4Qa3XDqoUIxtia5o19OptfHIeQccF1kWapk9yRztFlafnwoE4i', true, 5, 'HR',1 );
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `employee_id`, `role`,`is_supervisor`) VALUES (3, 'areti', '$2a$12$HbC4Qa3XDqoUIxtia5o19OptfHIeQccF1kWapk9yRztFlafnwoE4i', true, 3, 'Employee',0 );
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `employee_id`, `role`,`is_supervisor`) VALUES (4, 'kleftis', '$2a$12$HbC4Qa3XDqoUIxtia5o19OptfHIeQccF1kWapk9yRztFlafnwoE4i', true, 4, 'Employee',0 );
