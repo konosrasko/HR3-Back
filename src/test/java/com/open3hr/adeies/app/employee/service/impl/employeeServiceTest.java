7a5080ffee4544b9bd8e43142fd7b4ce9ac326b5
Konstantinos Raskopoulos <kraskopoulos@ots.gr>
2023-08-18 11:13:49 +0300

    declineLeaveRequest and findEmployeeByUserName Tests

--- src/test/java/com/open3hr/adeies/app/employee/service/impl/employeeServiceTest.java
+++ src/test/java/com/open3hr/adeies/app/employee/service/impl/employeeServiceTest.java
@@ -2,12 +2,18 @@ package com.open3hr.adeies.app.employee.service.impl;
 
 import com.open3hr.adeies.app.employee.entity.Employee;
 import com.open3hr.adeies.app.employee.repository.EmployeeRepository;
+import com.open3hr.adeies.app.enums.Role;
+import com.open3hr.adeies.app.enums.Status;
 import com.open3hr.adeies.app.leaveBalance.entity.LeaveBalance;
 import com.open3hr.adeies.app.leaveBalance.repository.LeaveBalanceRepository;
 import com.open3hr.adeies.app.leaveCategory.entity.LeaveCategory;
 import com.open3hr.adeies.app.leaveCategory.repository.LeaveCategoryRepository;
 import com.open3hr.adeies.app.leaveRequest.dto.LeaveRequestDTO;
+import com.open3hr.adeies.app.leaveRequest.entity.LeaveRequest;
 import com.open3hr.adeies.app.leaveRequest.repository.LeaveRequestRepository;
+import com.open3hr.adeies.app.user.entity.User;
+import com.open3hr.adeies.app.user.repository.UserRepository;
+import com.open3hr.adeies.app.user.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,6 +28,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 
@@ -36,17 +44,22 @@ import static org.mockito.Mockito.when;
 
         @Mock
         private LeaveRequestRepository leaveRequestRepository;
+        @Mock
+        private UserRepository userRepository;
 
         @InjectMocks
         private EmployeeServiceImpl employeeService;
 
+
         @Mock
         private LeaveBalanceRepository leaveBalanceRepository;
         private Employee employee;
+        private User user;
         private LeaveRequestDTO leaveRequestDTO;
 
         @BeforeEach
         public void init() throws ParseException {
+
 
 
             employee = Employee.builder()
@@ -67,6 +80,16 @@ import static org.mockito.Mockito.when;
                     .build());
             employee.setLeaveBalanceList(leaveBalances);
 
+            user = User.builder()
+                    .id(1)
+                    .employee(employee)
+                    .isEnable(true)
+                    .role(Role.HR)
+                    .username("test")
+                    .password("test")
+                    .isSupervisor(false)
+                    .build();
+
             leaveRequestDTO= LeaveRequestDTO.builder()
                     .id(1)
                     .leaveTitle("Normal")
@@ -76,21 +99,52 @@ import static org.mockito.Mockito.when;
                     .status(null)
                     .endDate(new SimpleDateFormat("yyyy-MM-dd").parse("2023-05-26"))
                     .build();
+
+
         }
 
         @Test
         void addNewLeaveRequest()
         {
-
-            when(employeeRepository.findById(employee.getId())).thenReturn(Optional.of(employee));
             LeaveCategory leaveCategory = new LeaveCategory(1,"Normal");
+            when(employeeRepository.findById(employee.getId())).thenReturn(Optional.of(employee));
             when(leaveCategoryRepository.findCategoryByTitle(leaveRequestDTO.getLeaveTitle())).thenReturn(Optional.of(leaveCategory));
             LeaveRequestDTO answer = employeeService.addLeaveRequest(leaveRequestDTO,employee.getId());
 
-
-            Assertions.assertEquals("PENDING",answer.getStatus().toString());
+            assertEquals("PENDING",answer.getStatus().toString());
             Assertions.assertNotNull(answer);
 
         }
+
+        @Test
+        void findEmployeeByUserName()
+        {
+            when(userRepository.findUserByUsername(user.getUsername())).thenReturn(Optional.of(user));
+            when(employeeRepository.findById(employee.getId())).thenReturn(Optional.of(employee));
+            Employee employee1 = new Employee(employeeService.findEmployeeByUserName(user.getUsername()));
+            assertEquals(employee1.getId(),employee.getId());
+        }
+
+        @Test
+        void declineLeaveRequest() throws ParseException {
+
+
+            LeaveCategory leaveCategory = new LeaveCategory(1,"Normal");
+            LeaveRequest leaveRequest = LeaveRequest.builder()
+                    .status(Status.PENDING)
+                    .id(1)
+                    .employee(employee)
+                    .startDate(new SimpleDateFormat("yyyy-MM-dd").parse("2023-05-24"))
+                    .endDate(new SimpleDateFormat("yyyy-MM-dd").parse("2023-05-26"))
+                    .submitDate(new SimpleDateFormat("yyyy-MM-dd").parse("2023-05-22"))
+                    .category(leaveCategory)
+                    .duration(2)
+                    .build();
+            when(leaveRequestRepository.findById(leaveRequest.getId())).thenReturn(Optional.of(leaveRequest));
+            LeaveRequestDTO leaveRequestDTO1 = employeeService.declineLeaveRequest(leaveRequest.getId());
+            assertEquals(Status.DENIED,leaveRequestDTO1.getStatus());
+        }
+
+
     }
 
